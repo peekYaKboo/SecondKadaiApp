d49898fbc895c7cdd0a95dd16c661ad6b8520428
yuki <marukesu11@icloud.com>
2019-03-31 16:41:22 +0900

    UITextFieldの入力内容を取得し、表示

---
+++ SecondKadaiApp/ResultViewController2.swift
@@ -0,0 +1,34 @@
+//
+//  ResultViewController2.swift
+//  SecondKadaiApp
+//
+//  Created by 川田有紀 on 2019/03/31.
+//  Copyright © 2019 jp.techacademy.yuki.kawata. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController2: UIViewController {
+    @IBOutlet weak var label: UILabel!
+    var s = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        label.text = "\(s) さんこんにちは"
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var name: UITextField!
+    
+    var nameString = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    @IBAction func unwind(_ segue: UIStoryboardSegue){
+        
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+        
+        let resultViewController2:ResultViewController2 = segue.destination as! ResultViewController2
+        resultViewController2.s = name.text!
+        
+        name.text = ""
+        
+    }
 }
 
